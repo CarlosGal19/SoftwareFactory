b7ea309f440c16dcfd7af6d48ed626be5c73b235
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-21 13:31:14 -0600

    fix: remove db name from environment variables

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'root', process.env.DB_PASS, (
+const sequelize = new Sequelize('utma', 'root', process.env.DB_PASS, (
     {
         host: process.env.DB_HOST,
         dialect: 'mariadb'

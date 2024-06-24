002b5c4ae013be688343555150d2ce17fdfbd16a
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-23 22:09:48 -0600

    feat: add swagger config

---
+++ swaggerConfig/swagger.js
@@ -0,0 +1,27 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'UTMA NETWORK API',
+            version: '1.0.0'
+        },
+    },
+    apis: ['../routes/*.js', '../models/*.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerDocs = (app, port) => {
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.get('/api/docs.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+
+    console.log(`Swagger running on port ${port}`);
+}
+
+module.exports = swaggerDocs;

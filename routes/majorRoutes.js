eb4f38ae41552173e30bc0220e7e9cd47f029541
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-23 22:28:38 -0600

    feat: add validation from admins

--- routes/majorRoutes.js
+++ routes/majorRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const { addMajor, getMajors, getMajor } = require('../controllers/majorController.js')
-// const authMiddleware = require('../middlewares/authMiddleware.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', authMiddleware, (req, res) => {
     addMajor(req, res);
 });
 
-router.get('/', (req, res) => {
+router.get('/', authMiddleware, (req, res) => {
     getMajors(req, res);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', authMiddleware, (req, res) => {
     getMajor(req, res);
 });
 
--- routes/userTypeRoutes.js
+++ routes/userTypeRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const { addUserType, getUserTypes, getUserType } = require('../controllers/userTypeController.js')
-// const authMiddleware = require('../middlewares/authMiddleware.js');
+const { addUserType, getUserTypes, getUserType } = require('../controllers/userTypeController.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', authMiddleware, (req, res) => {
     addUserType(req, res);
 });
 
-router.get('/', (req, res) => {
+router.get('/', authMiddleware, (req, res) => {
     getUserTypes(req, res);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', authMiddleware, (req, res) => {
     getUserType(req, res);
 });
 

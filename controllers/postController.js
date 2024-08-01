70de90ce5a9a0b5cb85b7b4af7a23f02b08c25f2
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-01 07:54:50 -0600

    feat: add validation to edit only your posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -74,6 +74,7 @@ const createPost = async (req, res) => {
 const updatePost = async (req, res) => {
     try {
         const id = req.params.id;
+        const editor = req.user.id;
         const { content, url_img, title } = req.body;
 
         if (!id) return res.status(400).send({ message: 'Please provide the post id.' });
@@ -86,7 +87,9 @@ const updatePost = async (req, res) => {
         if (!post) {
             return res.status(404).send({ message: 'Post not found' });
         }
-
+        if(post.creator_id !== editor) {
+            return res.status(403).send({ message: 'You are not allowed to update this post' });
+        }
         post.content = content;
         post.title = title;
         post.url_img = url_img;

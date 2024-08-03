f2ad71edf9417ab341cb918ef697686ce30fa18a
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-02 20:02:44 -0600

    refactor: use only id for generating jwt

--- helpers/generateJWT.js
+++ helpers/generateJWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const generateJWT = (id, name, type) => {
-    return jwt.sign({ id, name, type }, process.env.JWT_SECRET, {
+const generateJWT = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: '7d'
     });
 }

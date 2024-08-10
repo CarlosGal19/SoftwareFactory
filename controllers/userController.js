6a84a9ff4bd1280a8c72edaf57f59f1099e47814
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-10 17:23:10 -0600

    feat: add default major

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,8 +20,9 @@ const addUser = async (req, res) => {
         if (userExists) {
             return res.status(400).send({ message: 'User already exists.' });
         }
-        const { name, last_name, birth_date, gender, major_id, password } = req.body;
+        const { name, last_name, birth_date, gender, password } = req.body;
         const user_type_id = req.body.user_type_id || 2;
+        const major_id = req.body.major_id || 1;
         if ([user_name, name, last_name, birth_date, gender, user_type_id, major_id, email, password].includes(undefined)) {
             return res.status(400).send({ message: 'Please provide all the required fields.' });
         }

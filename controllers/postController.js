69a9f0f64fe1ec740ddc0ef1944a5bc23cbafe8b
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-13 11:54:25 -0600

    fix: update option to status

--- controllers/postController.js
+++ controllers/postController.js
@@ -176,13 +176,13 @@ const getNoValidatedPosts = async (req, res) => {
 const validatePost = async (req, res) => {
     try {
         const post_id = req.params.id;
-        const option = req.body.option;
+        const status = req.body.status;
         if (!post_id) return res.status(400).send({ message: 'Please provide the post id.' });
         const post = await Post.findByPk(post_id);
         if (!post) {
             return res.status(404).send({ message: 'Post not found' });
         }
-        if (option === 'rejected') {
+        if (status === 'rejected') {
             await post.destroy();
             return res.status(200).send({ message: 'Post rejected successfully' });
         }
